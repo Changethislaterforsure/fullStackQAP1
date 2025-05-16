d57a1d3672626702414a05baf262c834974862a7
Changethislaterforsure <benjmain.legge@keyin.com>
2025-05-16 14:52:49 -0230

    Update passwordGenerator.test.js
    created first test

--- tests/passwordGenerator.test.js
+++ tests/passwordGenerator.test.js
@@ -2,6 +2,8 @@ const { generatePassword, parseArguments } = require("../passwordGenerator");
 
 describe('generatePassword', () => {
     test('The generated password is of the correct length', () => {
+        const { password } = generatePassword(12, true, false, false, false);
+        expect(password).toHaveLength(12);
 
     });
 })
